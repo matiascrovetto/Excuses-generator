d3733075929b72bb95f3546b6dcfc1b4cbc5b633
matiascrovetto <matiastapiacrovetto@gmail.com>
2023-02-02 05:25:11 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let quien = ["El gato", "Mi vecino", "Mi mama"];
+  let que = ["se estrello", "comio", "bebio", "me visito"];
+  let cuando = ["anteayer", "en mi casa", "en la escuela", "hoy"];
+
+  let quienIndx = Math.floor(Math.random() * quien.length);
+  let queIndx = Math.floor(Math.random() * que.length);
+  let cuandoIndx = Math.floor(Math.random() * cuando.length);
+
+  return quien[quienIndx] + " " + que[queIndx] + " " + cuando[cuandoIndx];
 };
